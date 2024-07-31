e7541506a3c3546e70fd9a7f787c42235f9c85de
Remi Cabande <remi.cabande@epitech.eu>
2024-07-31 17:23:16 +0200

    Added gitignore, Added simple implementation of espress server that receives a request from frontend, and answers back

--- server_dir/index.js
+++ server_dir/index.js
@@ -12,21 +12,18 @@ app.use(express.json());
 
 app.get('/api/data', async (req, res) => {
   try {
+	console.log("GET /api/data");
+	res.status(200).json({id: "0", name: "Song 1"});
   } catch (error) {
     console.error('Error: ', error.message);
     res.status(500).json({ error: 'Error has occured' });
   }
 });
 
-const compareTimestamp = (a, b) => {
-  return a === b;
-};
-
-
 
 app.post('/api', async (req, res) => {
   try {
-      //res.json(JSON.parse(updatedContent))
+	console.log("POST /");
   } catch (error) {
     console.error('Error: ', error.message);
     res.status(500).send('Error has occured');
--- web_dir/lib/main.dart
+++ web_dir/lib/main.dart
@@ -1,4 +1,6 @@
+import 'dart:convert';
 import 'dart:developer';
+import 'dart:io';
 import 'dart:ui_web';
 
 import 'package:assets_audio_player/assets_audio_player.dart';
@@ -6,6 +8,7 @@ import 'package:assets_audio_player_web/web/assets_audio_player_web.dart';
 import 'package:flutter/material.dart';
 import 'package:assets_audio_player_web/assets_audio_player_web.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:http/http.dart' as http;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -34,7 +37,6 @@ class MyHomePage extends ConsumerStatefulWidget {
   const MyHomePage({super.key, required this.title});
 
   final String title;
-  //final JustAudioPlayer audioPlayer; 
 
   @override
   ConsumerState<ConsumerStatefulWidget> createState() => _MyHomePageState();
@@ -43,6 +45,7 @@ class MyHomePage extends ConsumerStatefulWidget {
 class _MyHomePageState extends ConsumerState<MyHomePage> {
   @override
   Widget build(BuildContext context) {
+    // Instance of the audio player class
     final assetsAudioPlayer = AssetsAudioPlayer();
     return Scaffold(
       appBar: AppBar(
@@ -58,18 +61,27 @@ class _MyHomePageState extends ConsumerState<MyHomePage> {
           IconButton(onPressed:() async {
               log("Playback started");
               try {
-                await assetsAudioPlayer.open(
-                  Audio("assets/audio/Vivid.mp3"),
-                  showNotification: true,
-                );
-                assetsAudioPlayer.play();
+                // Issuing HTTP request to server
+                http.Response res = await http.get(Uri.parse("http://192.168.0.173:3001/api/data"));
+
+                log(res.body.toString());
+
+                if (res.statusCode == 200) {
+                  //  await assetsAudioPlayer.open(
+                  //    Audio("assets/audio/Vivid.mp3"),
+                  //    showNotification: true,
+                  //  );
+                  //  assetsAudioPlayer.play();
+                }
               } catch (e) {
                 log(e.toString());
               }
             }, icon: Icon(Icons.play_arrow)),
           IconButton(onPressed:() async {
               log("Playback stopped");
-              await assetsAudioPlayer.stop();
+              if (assetsAudioPlayer.isPlaying.value) {
+                await assetsAudioPlayer.stop();
+              }
             }, icon: Icon(Icons.pause)),
         ],
       ),)
